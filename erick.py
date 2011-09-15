341886a83da7d35f0ebe83fc8c693e5df295198d
e0x <samudhio@gmail.com>
2011-09-15 16:24:46 -0400

    overhaul of everything

--- erick.py
+++ erick.py
@@ -8,49 +8,78 @@ class AppFrame( wx.Frame ):
     def __init__(self, title):
         
         wx.Frame.__init__( self, None, wx.ID_ANY, title=title, size=(800, 600) )
-        self.frmPanel = wx.Panel( self )
-        self.frmPanel.BackgroundColour = (200, 240, 250) # light blue
-
-        # Add another panel 
-        self.BtnPanel = wx.Panel(self.frmPanel)
-        self.BtnPanel.SetBackgroundColour( wx.NamedColor( 'GREY' ) )
+        self.Panel = wx.Panel( self )
+        #self.Panel.BackgroundColour = (200, 240, 250) # light blue   
 
         # Add Buttons
-        self.Querybtn = wx.Button(self.frmPanel, label="Consultar")
-        self.Addbtn = wx.Button(self.frmPanel, label="agregar")
-        self.Savebtn = wx.Button(self.frmPanel, label="Salvar")
-        self.Clearbtn = wx.Button(self.frmPanel, label="Limpiar")
-        self.Delbtn = wx.Button(self.frmPanel, label="Borrar")
+        self.Querybtn = wx.Button(self.Panel, label="Consultar", pos=(1, 5))
+        self.Addbtn = wx.Button(self.Panel, label="agregar")
+        self.Savebtn = wx.Button(self.Panel, label="Salvar")
+        self.Clearbtn = wx.Button(self.Panel, label="Limpiar")
+        self.Delbtn = wx.Button(self.Panel, label="Borrar")
+        
+        # Add static text and input fields and they own Sizer
+        self.Ced_vSizer = wx.BoxSizer(wx.VERTICAL)
+        self.CedTxt = wx.StaticText(self.Panel, wx.ID_ANY, 'Cedula:')
+        self.CedInput = wx.TextCtrl(self.Panel, wx.ID_ANY)
+        self.Ced_vSizer.Add(self.CedTxt, proportion=0, flag=wx.EXPAND|wx.ALL, border=5)
+        self.Ced_vSizer.Add(self.CedInput, proportion=1, flag=wx.EXPAND|wx.ALL, border=5)
+        self.Ced_vSizer.Add(self.Querybtn, proportion=1, flag=wx.EXPAND|wx.ALL, border=5) 
+        
+        # Add Static text and Text Ctlr of name , tel, Address and they own Sizer
+        self.NameText = wx.StaticText(self.Panel, wx.ID_ANY, 'Nombre:')
+        self.NameInput = wx.TextCtrl(self.Panel, wx.ID_ANY, size = wx.Size(400, 20))
+        self.DirText = wx.StaticText(self.Panel, wx.ID_ANY, 'Direccion:')
+        self.DirInput = wx.TextCtrl(self.Panel, wx.ID_ANY, size = wx.Size(400, 20))
+        self.TelText  = wx.StaticText(self.Panel, wx.ID_ANY, 'Telefono:')
+        self.TelInput = wx.TextCtrl(self.Panel, wx.ID_ANY, size = wx.Size(100, 20))
+        self.DataFields = wx.FlexGridSizer(rows = 3, cols = 2, hgap = 5, vgap = 10)
+        self.DataFields.AddMany([(self.NameText, 0, wx.ALIGN_CENTER_VERTICAL),
+                                  (self.NameInput, 0, wx.ALIGN_CENTER_VERTICAL),
+                                  (self.DirText, 0, wx.ALIGN_CENTER_VERTICAL),
+                                  (self.DirInput, 1, wx.EXPAND),
+                                  (self.TelText, 0, wx.ALIGN_CENTRE_VERTICAL),
+                                  self.TelInput])
+        
+        # Add list crtl
+        self.listctrl = wx.ListCtrl(self.Panel, style=wx.LC_REPORT|wx.BORDER_SUNKEN)
+        self.listctrl.InsertColumn(0, 'ID')
+        self.listctrl.InsertColumn(1, 'Detalle', width=wx.EXPAND)
+        
+        # Add textcrtl for add more details
+        self.AddTextCrtl = wx.TextCtrl(self.Panel, -1, '', style=wx.MULTIPLE)
 
-        # Add panel to a sizer
-        PnlAndBtn_vSizer = wx.BoxSizer( wx.VERTICAL )
-        PnlAndBtn_vSizer.Add( self.BtnPanel, 1, wx.EXPAND|wx.ALL, 1)
+        # Add Sizer's for layout managment
+        self.mainSizer = wx.BoxSizer(wx.VERTICAL)
+        self.topSizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.midSizer = wx.BoxSizer(wx.VERTICAL)
+        self.bottomSizer = wx.BoxSizer(wx.HORIZONTAL)
+        
 
+
+        # Add elements to the top sizer
+        self.topSizer.Add(self.Ced_vSizer, 0, wx.ALIGN_LEFT)
+        self.topSizer.Add(self.DataFields, 0, wx.ALIGN_LEFT)
+        self.mainSizer.Add(self.topSizer, proportion=1, flag=wx.ALL, border=5)
+
+        # add list crtl to the own size
+        self.midSizer.Add(self.Addbtn, proportion=0, flag=wx.CENTER|wx.ALL, border=10)
+        self.midSizer.Add(self.AddTextCrtl, proportion=0, flag=wx.EXPAND|wx.ALL, border=5)
+        self.midSizer.Add(self.listctrl, proportion=1, flag=wx.EXPAND|wx.ALL)
+        self.mainSizer.Add(self.midSizer, proportion=2, flag=wx.EXPAND|wx.ALL, border=5)
         # Add buttons in the own sizer
-        btn_hSizer = wx.BoxSizer( wx.HORIZONTAL ) 
+
+        # Add elements to the bottom sizer
+        self.bottomSizer.Add(self.Delbtn, proportion=0, flag=wx.EXPAND|wx.ALL, border=5)
+        self.bottomSizer.Add(self.Clearbtn, proportion=0, flag=wx.EXPAND|wx.ALL, border=5)
+        self.bottomSizer.Add(self.Savebtn, proportion=0, flag=wx.EXPAND|wx.ALL, border=5)
+        self.mainSizer.Add(self.bottomSizer, proportion=1, flag=wx.EXPAND|wx.ALL, border=55)
+        # set hbox sizer to vbox sizer
+
+
+        # setsizer panel
+        self.Panel.SetSizer(self.mainSizer)
         
-        btn_hSizer.Add(self.Querybtn, proportion=0,
-                flag=wx.ALIGN_LEFT|wx.ALL, border=5)
-
-        btn_hSizer.AddStretchSpacer()
-
-        btn_hSizer.Add(self.Addbtn, proportion=0, flag=wx.EXPAND|wx.ALL, 
-                border=5)
-        btn_hSizer.Add(self.Savebtn, proportion=0, flag=wx.EXPAND|wx.ALL,
-                border=5)
-        btn_hSizer.Add(self.Clearbtn, proportion=0, flag=wx.EXPAND|wx.ALL,
-                border=5)
-        btn_hSizer.Add(self.Delbtn, proportion=0, flag=wx.EXPAND|wx.ALL,
-                border=5)
-        
-        btn_hSizer.AddStretchSpacer()
-
-        PnlAndBtn_vSizer.Add( btn_hSizer, 0, wx.EXPAND|wx.ALL, 5 )
-
-        # SetSizer both sizers in the most senior control that has sizer in it.
-        self.frmPanel.SetSizer(PnlAndBtn_vSizer)
-        self.frmPanel.Layout()
-
         # Must call before any event handler is referenced.
         self.eventsHandler = EventsHandler(self)
 
@@ -68,11 +97,18 @@ class AppFrame( wx.Frame ):
 
 class EventsHandler():
     
+
     def __init__(self, parent):
         self.parent = parent
+        self.index = 0
+        self.ID = 1
 
     def OnAddBtn(self, event):
-        pass
+        self.parent.listctrl.InsertStringItem(self.index, str(self.ID))
+        self.parent.listctrl.SetStringItem (self.index, 1, self.parent.AddTextCrtl.GetValue()) 
+        self.index += 1
+        self.ID += 1
+        self.parent.AddTextCrtl.Clear()
 
     def OnQueryBtn(self, event):
         pass
@@ -100,3 +136,4 @@ def Main():
 
 if __name__ == '__main__':
     Main()
+
